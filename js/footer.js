67e5d75acbd7054e8b6b30bccce685bbd5a0a452
Anja Mehlhase <anja.mehlhase@gmail.com>
2023-04-11 19:04:18 +0200

    added dynamic footer

---
+++ js/footer.js
@@ -0,0 +1,14 @@
+document.writeln(
+  `  
+    <p class="social">
+        <a href="https://www.etsy.com/" target="_blank"><i class="fa fa-etsy" aria-hidden="true"></i></a>
+        <a href="https://de-de.facebook.com/" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+        <a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        <a href="https://www.tumblr.com/" target="_blank"><i class="fa fa-tumblr-square" aria-hidden="true"></i></a>
+        <a href="https://www.youtube.com/" target="_blank"><i class="fa fa-youtube-square" aria-hidden="true"></i></a>
+    </p>
+    <p class="copyright">© ${new Date().getFullYear()}<br />
+    This website is a project by Anja Mehlhase<br />
+    Datura is a fictional webshop<br /><br />
+    All images are from <a href="https://unsplash.com/">Unsplash.com <i class="fa fa-heart-o" aria-hidden="true"></i></a></p>`
+);
