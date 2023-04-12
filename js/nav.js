a8fde9249eae6e33cfc443e57d43dfc1ee214718
Anja Mehlhase <anja.mehlhase@gmail.com>
2023-04-12 17:29:58 +0200

    added dynamic navigation

---
+++ js/nav.js
@@ -0,0 +1,26 @@
+document.writeln(
+  ` <ul>
+<li><a href="index.html">Start</a></li>
+<li>Angebot <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-right" aria-hidden="true"></i>
+  <ul class="dropdown">
+    <li><a href="#">Alle Pflanzen</a></li>
+    <li><a href="#">Individuelles Angebot</a></li>
+    <li><a href="../sites/memory.html">Memory</a></li>
+  </ul>
+</li>
+<li>Pflege <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-right" aria-hidden="true"></i>
+    <ul class="dropdown">
+      <li><a href="#">Unser Pflanzen 1x1</a></li>
+      <li><a href="#">Düngen und Erde</a></li>
+      <li><a href="#">Zubehör</a></li>
+    </ul>
+  </li>
+<li>About <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-right" aria-hidden="true"></i>
+    <ul class="dropdown">
+      <li><a href="../sites/about.html">Über diese Seite</a></li>
+      <li><a href="#">Kontakt</a></li>
+      <li><a href="../sites/impressum.html">Impressum</a></li>
+    </ul>
+  </li>
+</ul>`
+);
