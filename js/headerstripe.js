f4c9bd951aee2032daf64120cb9be26f1fb81952
Anja Mehlhase <anja.mehlhase@gmail.com>
2023-04-13 13:11:23 +0200

    made header-stripe dynamic and responsive, added dynamic nav to all HTML files

---
+++ js/headerstripe.js
@@ -0,0 +1,6 @@
+document.writeln(
+  `<div><i class="fa fa-truck" aria-hidden="true"></i> Gratis Lieferung</div>
+    <div class="mobilehidden"><i class="fa fa-star" aria-hidden="true"></i> Top-Bewertungen</div>
+    <div><i class="fa fa-envira" aria-hidden="true"></i> Klimaneutral</div>
+    <div class="mobilehidden"><i class="fa fa-phone" aria-hidden="true"></i> Persönliche Beratung</div>`
+);
