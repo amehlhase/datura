7137ccc2d028cff0454960ba35335d76d5256507
Anja Mehlhase <anja.mehlhase@gmail.com>
2024-08-10 21:33:00 +0200

    added links to pages in nav

--- js/nav.js
+++ js/nav.js
@@ -3,22 +3,22 @@ document.writeln(
 <li><a href="../index.html">Start</a></li>
 <li>Angebot <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-right" aria-hidden="true"></i>  
   <ul class="dropdown">
-    <li><a href="#">Alle Pflanzen</a></li>
-    <li><a href="#">Individuelles Angebot</a></li>
+    <li><a href="../sites/all-plants.html">Alle Pflanzen</a></li>
+    <li><a href="../sites/individual-offer.html">Individuelles Angebot</a></li>
     <li><a href="../sites/memory.html">Memory</a></li>
   </ul>
 </li>
 <li>Pflege <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-right" aria-hidden="true"></i>
     <ul class="dropdown">
-      <li><a href="#">Unser Pflanzen 1x1</a></li>
-      <li><a href="#">Düngen und Erde</a></li>
-      <li><a href="#">Zubehör</a></li>
+      <li><a href="../sites/1x1.html">Unser Pflanzen 1x1</a></li>
+      <li><a href="../sites/fertilizer-soil.html">Düngen und Erde</a></li>
+      <li><a href="../sites/tools.html">Zubehör</a></li>
     </ul>
   </li>
 <li>About <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-right" aria-hidden="true"></i>
     <ul class="dropdown">
       <li><a href="../sites/about.html">Über diese Seite</a></li>
-      <li><a href="#">Kontakt</a></li>
+      <li><a href="../sites/contact.html">Kontakt</a></li>
       <li><a href="../sites/impressum.html">Impressum</a></li>
     </ul>
   </li>
