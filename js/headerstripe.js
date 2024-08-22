b7cfe66f6d393a53f06b73ff0f3c1fcbd9afa9d6
Anja Mehlhase <anja.mehlhase@gmail.com>
2024-08-22 17:45:42 +0200

    cleaned up and sorted code in scss files, corrected responsiveness of newsletter teaser, shortened headerstripe

--- js/headerstripe.js
+++ js/headerstripe.js
@@ -1,5 +1,5 @@
 document.writeln(
   `<div><i class="fa fa-truck" aria-hidden="true"></i> Gratis Lieferung</div>
-    <div><i class="fa fa-envira" aria-hidden="true"></i> Klimaneutral</div>
+    <div><i class="fa fa-envira" aria-hidden="true"></i> CO2-neutral</div>
     <div class="mobilehidden"><i class="fa fa-phone" aria-hidden="true"></i> Persönliche Beratung</div>`
 );
