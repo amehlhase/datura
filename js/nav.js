f21bc2cb4dda62e6814386f554150b3330787c98
Anja Mehlhase <anja.mehlhase@gmail.com>
2024-09-01 13:41:31 +0200

    deleted empty pages from navigation, deleted pictures that were not used, replaced placeholder links in index.html

--- js/nav.js
+++ js/nav.js
@@ -1,19 +1,11 @@
 const nav = document.getElementById("nav");
 let navHtml = `<ul><li><a href="../index.html">Start</a></li>
-<li>Angebot <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-right" aria-hidden="true"></i>  
+<li>Pflanzen <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-right" aria-hidden="true"></i>  
   <ul class="dropdown">
     <li><a href="../sites/all-plants.html">Alle Pflanzen</a></li>
-    <li><a href="../sites/individual-offer.html">Individuelles Angebot</a></li>
     <li><a href="../sites/memory.html">Memory</a></li>
   </ul>
 </li>
-<li>Pflege <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-right" aria-hidden="true"></i>
-    <ul class="dropdown">
-      <li><a href="../sites/1x1.html">Unser Pflanzen 1x1</a></li>
-      <li><a href="../sites/fertilizer-soil.html">Düngen und Erde</a></li>
-      <li><a href="../sites/tools.html">Zubehör</a></li>
-    </ul>
-  </li>
 <li>About <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-right" aria-hidden="true"></i>
     <ul class="dropdown">
       <li><a href="../sites/about.html">Über diese Seite</a></li>
