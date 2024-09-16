ff05c7c3e74ae18030009fe0825ca6648b30ba6e
Anja Mehlhase <anja.mehlhase@gmail.com>
2024-09-16 11:28:03 +0200

    corrected lang attribute from en to de, changed file paths from ../ to /

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,16 @@
 const nav = document.getElementById("nav");
-let navHtml = `<ul><li><a href="../index.html">Start</a></li>
+let navHtml = `<ul><li><a href="/index.html">Start</a></li>
 <li>Pflanzen <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-right" aria-hidden="true"></i>  
   <ul class="dropdown">
-    <li><a href="../sites/all-plants.html">Alle Pflanzen</a></li>
-    <li><a href="../sites/memory.html">Memory</a></li>
+    <li><a href="/sites/all-plants.html">Alle Pflanzen</a></li>
+    <li><a href="/sites/memory.html">Memory</a></li>
   </ul>
 </li>
 <li>About <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-right" aria-hidden="true"></i>
     <ul class="dropdown">
-      <li><a href="../sites/about.html">Über diese Seite</a></li>
-      <li><a href="../sites/contact.html">Kontakt</a></li>
-      <li><a href="../sites/impressum.html">Impressum</a></li>
+      <li><a href="/sites/about.html">Über diese Seite</a></li>
+      <li><a href="/sites/contact.html">Kontakt</a></li>
+      <li><a href="/sites/impressum.html">Impressum</a></li>
     </ul>
   </li>
 </ul>`;
